183f4fdd6fb9c28a938cc285e782d00f9941518e
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-08-07 11:43:14 +0330

    skills section add and complete

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,9 +1,63 @@
 import './Skills.scss'
+import { technologies } from '../../constants'
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+
+const Ball = (props: {
+    imgUrl: string
+}) => {
+    const [decal] = useTexture([props.imgUrl]);
+    return (
+        <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+            <ambientLight intensity={0.25} />
+            <directionalLight position={[0, 0, 0.05]} />
+            <mesh castShadow receiveShadow scale={2.75}>
+                <icosahedronGeometry args={[1, 1]} />
+                <meshStandardMaterial
+                    color='#fff8eb'
+                    polygonOffset
+                    polygonOffsetFactor={-5}
+                    flatShading
+                />
+                <Decal
+                    position={[0, 0, 1]}
+                    rotation={[2 * Math.PI, 0, 6.25]}
+                    scale={1}
+                    map={decal}
+                />
+            </mesh>
+        </Float>
+    );
+};
+
+
+const BallCanvas = ({ icon }: { icon: string }) => {
+    return (
+        <Canvas
+            frameloop='demand'
+            dpr={[1, 2]}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense>
+                <OrbitControls enableZoom={false} />
+                <Ball imgUrl={icon} />
+            </Suspense>
+
+            <Preload all />
+        </Canvas>
+    );
+};
+
 
 export const Skills = () => {
     return (
         <div className="main-skills">
-            Skills
+            {technologies.map((technology) => (
+                <div className='ball-container' key={technology.name} id={technology.name}>
+                    <BallCanvas icon={technology.icon} />
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,79 @@
+import javascript from "../assets/tech/javascript.png"
+import typescript from "../assets/tech/typescript.png"
+import html from "../assets/tech/html.png"
+import css from "../assets/tech/css.png"
+import reactjs from "../assets/tech/reactjs.png"
+import tailwind from "../assets/tech/tailwind.png"
+import git from "../assets/tech/git.png"
+import figma from "../assets/tech/figma.png"
+import threejs from "../assets/tech/threejs.svg"
+import vue from "../assets/tech/vue.png"
+import vuetify from "../assets/tech/vuetify.png"
+import pinia from "../assets/tech/pinia.png"
+import net from "../assets/tech/net.png"
+import postgres from "../assets/tech/postgresql.png"
+import nuxt from "../assets/tech/nuxt.svg"
+
+
+export const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Vue",
+        icon: vue,
+    },
+    {
+        name: "nuxt",
+        icon: nuxt,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Pinia",
+        icon: pinia,
+    },
+    {
+        name: "Postgresql",
+        icon: postgres,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "vuetify",
+        icon: vuetify,
+    },
+    {
+        name: ".net",
+        icon: net,
+    },
+];
