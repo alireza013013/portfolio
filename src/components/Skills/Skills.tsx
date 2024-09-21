7eda8b92731730170139b2a173451ce2b5b6cbff
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-09-21 17:40:01 +0330

    skill change

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,51 +1,24 @@
 import './Skills.scss'
 import { technologies } from '../../constants'
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
-const Ball = (props: {
-    imgUrl: string
-}) => {
-    const [decal] = useTexture([props.imgUrl]);
+
+const Ball = ({ icon, color, randomDelay }: { icon: string, color: string, randomDelay: number }) => {
     return (
-        <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-            <ambientLight intensity={0.25} />
-            <directionalLight position={[0, 0, 0.05]} />
-            <mesh castShadow receiveShadow scale={2.75}>
-                <icosahedronGeometry args={[1, 1]} />
-                <meshStandardMaterial
-                    color='#fff8eb'
-                    polygonOffset
-                    polygonOffsetFactor={-5}
-                    flatShading
-                />
-                <Decal
-                    position={[0, 0, 1]}
-                    rotation={[2 * Math.PI, 0, 6.25]}
-                    scale={1}
-                    map={decal}
-                />
-            </mesh>
-        </Float>
-    );
-};
-
-
-const BallCanvas = ({ icon }: { icon: string }) => {
-    return (
-        <Canvas
-            frameloop='demand'
-            dpr={[1, 2]}
-            gl={{ preserveDrawingBuffer: true }}
-        >
-            <Suspense>
-                <OrbitControls enableZoom={false} />
-                <Ball imgUrl={icon} />
-            </Suspense>
-
-            <Preload all />
-        </Canvas>
+        <div className="wave-container">
+            <div className="wave" style={{
+                backgroundColor: color,
+                animationDelay: randomDelay + "s"
+            }}></div>
+            <div className="wave" style={{
+                backgroundColor: color,
+                animationDelay: randomDelay + 0.5 + "s"
+            }}></div>
+            <div className="wave" style={{
+                backgroundColor: color,
+                animationDelay: randomDelay + 1 + "s"
+            }}></div>
+            <img src={icon} alt="icon skill" />
+        </div>
     );
 };
 
@@ -55,7 +28,7 @@ const Skills = () => {
         <div className="main-skills" id='skills'>
             {technologies.map((technology) => (
                 <div className='ball-container' key={technology.name} id={technology.name}>
-                    <BallCanvas icon={technology.icon} />
+                    <Ball icon={technology.icon} color={technology.color} randomDelay={Math.random()} />
                 </div>
             ))}
         </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -19,61 +19,76 @@ export const technologies = [
     {
         name: "HTML 5",
         icon: html,
+        color: "#ff6d00"
     },
     {
         name: "CSS 3",
         icon: css,
+        color: "#0292db"
     },
     {
         name: "JavaScript",
         icon: javascript,
+        color: "#ffd600"
     },
     {
         name: "TypeScript",
         icon: typescript,
+        color: "white"
     },
     {
         name: "React JS",
         icon: reactjs,
+        color: "rgb(147 187 197)"
     },
     {
         name: "Vue",
         icon: vue,
+        color: "rgb(49 71 94)"
     },
     {
         name: "nuxt",
         icon: nuxt,
+        color: "rgb(115 199 165)"
     },
     {
         name: "Tailwind CSS",
         icon: tailwind,
+        color: "rgb(137 195 205)"
     },
     {
         name: "Pinia",
         icon: pinia,
+        color: "#ffce44"
     },
     {
         name: "Postgresql",
         icon: postgres,
+        color: "#2f6792"
     },
     {
         name: "Three JS",
         icon: threejs,
+        color: "#51525c"
     },
     {
         name: "git",
         icon: git,
+        color: "rgb(233 178 161)"
     },
     {
         name: "figma",
         icon: figma,
+        color: "white"
     },
     {
         name: "vuetify",
         icon: vuetify,
+        color: "rgb(113 145 181)"
     },
     {
         name: ".net",
         icon: net,
+        color: "#5027d5"
     },
 ];
